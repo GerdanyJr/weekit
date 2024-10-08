5ab7d9f60a53bd8a789d78303d50a278d07d9c68
GerdanyJr <asa_lindo@hotmail.com>
2024-10-08 10:36:37 -0300

    first commit

---
+++ src/main/java/com/github/gerdanyjr/weekit/WeekitApplication.java
@@ -0,0 +1,13 @@
+package com.github.gerdanyjr.weekit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeekitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/gerdanyjr/weekit/WeekitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gerdanyjr.weekit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeekitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
